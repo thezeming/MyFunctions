27171a697fea0290311da75e5b2bea9d8b6d1fac
Zeming Li <bq21242@bristol.ac.uk>
2022-01-01 17:28:32 +0000

    The sixth commit

--- Installation From Github.R
+++
@@ -1,3 +0,0 @@
-devtools::install_github("SOTON2947/MyFunctions",
-                         ref="master",
-                         auth_token = "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")
--- Useful commands.R
+++ Useful commands.R
@@ -6,3 +6,8 @@ devtools::document()
 # install and attach package
 devtools::install()
 library(MyFunctions)
+
+# installation from Github
+devtools::install_github("SOTON2947/MyFunctions",
+                         ref="master",
+                         auth_token = "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")
