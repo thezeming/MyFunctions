ce6ed25808bf3f7155866f9f84bf2b552d8c6bc2
Zeming Li <bq21242@bristol.ac.uk>
2022-11-30 10:20:22 +0000

    added new argument show_bps to assign.portfolios, also improved the function's output selection method.

--- R/assign.portfolios.R
+++ R/assign.portfolios.R
@@ -9,7 +9,7 @@
 #' @param break_var The variable that is used to compute breakpoints.
 #'
 #' @param sort_var The variable that is used to compute portfolio membership.
-#' 
+#'
 #' @param bps A numerical vector specifying customised preakpoints.
 #' It should contain only the breakpoints not the end values.
 #' For example, for a tercile sort, this value should be c(0.2, 0.8), not c(0, 0.2, 0.8, 1).
@@ -25,9 +25,12 @@
 #' @param bp_prefix Similar to P_prefix, this is the prefix to be placed in front of breakpoint numbers.
 #' This will be part of the column names for the breakpoint columns.
 #' The default is 'bp', which gives column names bp1, bp2, ...
-#' 
+#'
 #' @param type An integer between 1 and 9 selecting one of the nine quantile algorithms used by quantile().
 #' The default value is 2 which is the default algorithm used by SAS.
+#'
+#' @param show_bps A Boolean value indicating whether to retain breakpoints in the output dataframe.
+#' When FALSE, the breakpoints will not be shown and the argument bp_prefix will be ignored.
 #'
 #' @return A tibble that contains two types of columns (variables), one is the portfolio membership columns, the other is the breakpoint columns.
 #' Each portfolio membership column gives Boolean values indicating whether an asset belongs to that portfolio.
@@ -43,10 +46,14 @@ assign.portfolios <-
            n_portfolios = 10,
            P_prefix = 'P',
            bp_prefix = 'bp',
+           show_bps = TRUE,
            type = 2) {
-    if (is.null(bps)){
+    OriginalVars <-
+      colnames(data)
+    
+    if (is.null(bps)) {
       breakpoints_probs <- seq(0, 1, length.out = n_portfolios + 1)
-    }else{
+    } else{
       breakpoints_probs <- c(0, bps, 1)
     }
     
@@ -61,24 +68,35 @@ assign.portfolios <-
       type = type)) |>
       pull(breakpoint) |>
       as.numeric()
-
-    for(i in 1:n_portfolios){
+    
+    for (i in 1:n_portfolios) {
       data <-
         data |>
-        mutate('{P_prefix}{i}':= if_else({{ sort_var }} >= breakpoints[i] & {{ sort_var }} <= breakpoints[i+1],
-                                TRUE,
-                                FALSE))
+        mutate('{P_prefix}{i}' := if_else({
+          {
+            sort_var
+          }
+        } >= breakpoints[i] & {
+          {
+            sort_var
+          }
+        } <= breakpoints[i + 1],
+        TRUE,
+        FALSE))
     }
-
-    for(i in 1:length(breakpoints)){
-      data <-
-        data |>
-        mutate('{bp_prefix}{i}':=breakpoints[i])
+    
+    if (show_bps) {
+      for (i in 1:length(breakpoints)) {
+        data <-
+          data |>
+          mutate('{bp_prefix}{i}' := breakpoints[i])
+      }
+      
     }
-
+    
     Dat <-
       data |>
-      select(paste0(P_prefix, 1):paste0(bp_prefix, length(breakpoints)))
-
+      select(-all_of(OriginalVars))
+      
     return(Dat)
   }
